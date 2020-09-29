5afe3febdaa915afc1ffff9d866a2d5353a463dd
babusot <milos.mitrev@yahoo.com>
2020-09-29 23:23:11 +0200

    Updates

---
+++ precache-manifest.55b0b34d101287354f07260fac27bf77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2817c7cbfa6b58afb9de0fbad0cb1fa7",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "9e792deddae3c8f66d62",
+    "url": "/weather-app/static/css/main.cfd2f8dd.chunk.css"
+  },
+  {
+    "revision": "02428de678194214389d",
+    "url": "/weather-app/static/js/2.13519f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.13519f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e792deddae3c8f66d62",
+    "url": "/weather-app/static/js/main.6e20a8e1.chunk.js"
+  },
+  {
+    "revision": "bdcb51b63d9249268df7",
+    "url": "/weather-app/static/js/runtime-main.f1c906e7.js"
+  }
+]);
